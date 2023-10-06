d9d7204471669ce0ca5db5afb5fc6e23d7f4f542
Mike <jones@cs.byu.edu>
2023-10-05 20:37:02 -0600

    hides subcategories

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -2,13 +2,22 @@
 import React from 'react';
 
 function Sidebar({ categories, onSelectCategory }) {
+  const [expandedCategory, setExpandedCategory] = React.useState(null);
+
+  const handleCategoryClick = (category) => {
+    // If the same category is clicked while it's expanded, collapse it. 
+    // Otherwise, expand the clicked category.
+    setExpandedCategory(current => current === category.name ? null : category.name);
+    onSelectCategory(category);
+  };
+
   return (
     <div className="sidebar">
       <ul>
         {categories.map((category) => (
-          <li key={category.name} onClick={() => onSelectCategory(category)}>
+          <li key={category.name} onClick={() => handleCategoryClick(category)}>
             {category.name}
-            {category.subcategories && (
+            {expandedCategory === category.name && category.subcategories && (
               <ul>
                 {category.subcategories.map((subcategory) => (
                   <li key={subcategory} onClick={(e) => {
